8a609a3abf28617357aa9f6f93a72240b59766b7
writes <jonathonhthompson@gmail.com>
2018-02-20 16:39:33 -0600

    feat(deploy): deploy to rinkeby network with infura

--- deploy.js
+++ deploy.js
@@ -9,3 +9,17 @@ const provider = new HDWalletProvider(
 );
 
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi There!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
