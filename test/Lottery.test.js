3b580cf7bdf277eecb051235ccc80f07baaa0f77
writes <jonathonhthompson@gmail.com>
2018-02-22 16:01:25 -0600

    feat(test): test min amount needed to enter

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -74,4 +74,15 @@ describe('Lottery Contract', () => {
     assert.equal(accounts[2], players[2]);
     assert.equal(3, players.length);
   });
+
+  it('requires a min amount of ether to enter', async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: '200',
+      });
+    } catch (err) {
+      assert.ok(err);
+    }
+  });
 });
