95689a3b04c6ed0aeff8ed7f63740ef96ccf589d
writes <jonathonhthompson@gmail.com>
2018-02-22 15:54:28 -0600

    feat(test): lottery test for enter function

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,23 +1,51 @@
 const assert = require('assert');
+const sinon = require('sinon');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+
 const web3 = new Web3(ganache.provider());
 
 const { interface, bytecode } = require('../compile');
 
 let lottery;
 let account;
+// let sandbox;
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
+
+  // sandbox = sinon.sandbox.create();
+
+  // sandbox
+  //   .stub(web3.eth.Contract.prototype, 'deploy')
+  //   .returns('expected returns');
+  // sandbox.stub(web3.eth.Contract.prototype, 'send').returns('expected returns');
 
   lottery = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: '1000000' });
 });
 
+// afterEach(async () => {
+//   sinon.sandbox.restore();
+// });
+
 describe('Lottery Contract', () => {
   it('deploys contract', () => {
     assert.ok(lottery.options.address);
+  });
+
+  it('allows one account to enter', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('.02', 'ether'),
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(1, players.length);
   });
 });
