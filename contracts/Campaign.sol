4e7722685e82ada7da814d8c3515311afcfca587
writes <jonathonhthompson@gmail.com>
2018-02-24 11:21:32 -0600

    Kickstarter branch for side project

---
+++ contracts/Campaign.sol
@@ -0,0 +1,17 @@
+/*
+Variables
+
+manager - owner of contract
+minimumContribution - min to enter
+!!!! approvers - approvers of payments to manager
+requests - list of requests the manager has created
+
+
+Functions
+
+Campaign
+contribute
+createRequest
+!!!! approveRequest
+finalizeRequest
+ */
