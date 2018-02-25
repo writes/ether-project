7005e9bd58a8dfc2e061a173aa96ef490db70a16
writes <jonathonhthompson@gmail.com>
2018-02-24 23:07:26 -0600

    feat(contract): approval function

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -34,13 +34,27 @@ contract Campaign {
 
     function createRequest(string description, uint value, address recipient) public restricted {
         // create new variable of type Request named newRequest which equals an instance of a struct
-        Request newRequest = Request({
+        Request memory newRequest = Request({
            description: description,
            value: value,
            recipient: recipient,
-           complete: false
+           complete: false,
+           approvalCount: 0
         });
 
         requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+      Request storage request = requests[index];
+
+      // check if sender is a donor
+      require(approvers[msg.sender]);
+      // check that sender has NOT already voted
+      require(!request.approvals[msg.sender]);
+
+      // set approver to true and increase the total approval account
+      request.approvals[msg.sender] = true;
+      request.approvalCount++;
     }
 }
